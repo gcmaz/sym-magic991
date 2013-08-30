115c2de1448fcb187bc4ae55dc52adc2ff337867
Stacy Mark <stacy.mark@kaff.com>
2013-08-30 08:19:50 -0700

    updated potw sponsor

--- src/Magic991/MainBundle/Controller/SplashController.php
+++ src/Magic991/MainBundle/Controller/SplashController.php
@@ -11,10 +11,15 @@ class SplashController extends Controller
         switch($splash){
             // COULD RETURN LIST OF SPLASH PAGES
             case 'default' :
-                return $this->render('Magic991MainBundle:Page:splashpage.html.twig', array(
+                return $this->render('Magic991MainBundle:Splash:default.html.twig', array(
                     'splash' => $splash
                 ));
-                
+
+            // TEACHERS APPRECIATION NIGHT
+            case 'teachers-appreciation-night' :
+                return $this->render('Magic991MainBundle:Splash:teachersnight.html.twig', array(
+                    'splash' => $splash
+                ));                
         }
     }
     
