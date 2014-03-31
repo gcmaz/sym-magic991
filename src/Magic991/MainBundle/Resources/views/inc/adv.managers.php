25babb58e10bd9ae20ad505c04f93c0d847cfac8
Stacy office dev <stacy.mark@kaff.com>
2014-03-31 11:16:42 -0700

    update sales contacts and internet info

--- src/Magic991/MainBundle/Resources/views/inc/adv.managers.php
+++ src/Magic991/MainBundle/Resources/views/inc/adv.managers.php
@@ -1,86 +1,32 @@
-<table cellpadding="2" cellspacing="2" style="margin:0px auto 20px;border:1px solid #fff;background-color:#fff;line-height:1.5;font-size:1em;width:90%;">
+<table cellpadding="2" cellspacing="2" style="margin:20px auto 20px;padding:20px 0;background-color:#fff;line-height:1.5;font-size:1em;width:90%;">
     <tr>
-      <td colspan="2" align="left"><p style="font-weight:bold;font-size:14px;margin:10px;line-height:20px;text-align:center;">For More Information Contact One Of<br/>The Following Marketing Managers</p></td>
-    </tr>
-    <tr bgcolor="#c9e5f2">
-      <td colspan="2" align="center" style="border:1px solid #fff;">
-            <span style="font-weight:bold;margin-left:20px;font-size:10px;letter-spacing:1.5;color:#336699;">PRESCOTT</span>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <div class="adinfodiv1">
-            <span style="font-weight:bold;">Patrick Gallagher</span><br />(928) 445-6880 ext 1<br />
-            <a href="mailto:patrick@magic991.com">patrick@magic991.com</a>
-        </div>
-      </td>
-      <td>
-        <div class="adinfodiv1">
-            <span style="font-weight:bold;">Patti Ezell</span><br />(928) 420-0525<br />
-            <a href="mailto:patti@magic991.com">patti@magic991.com</a>
-        </div>
+      <td colspan="3">
+          <p style="font-weight:bold;font-size:14px;margin:20px auto;text-align:center;">
+              For More Information Contact One Of The Following Marketing Managers
+          </p>
       </td>
     </tr>
     <tr>
       <td>
-        <div class="adinfodiv1">
-            <span style="font-weight:bold;">Dianne Valenti-Gallery</span><br />(928) 445-6880<br />
-            <a href="mailto:dianne@magic991.com">dianne@magic991.com</a>
-        </div>
-      </td>
-      <td>
-        <div class="adinfodiv1">
-            <span style="font-weight:bold;"></span><br /><br />
-        </div>
-      </td>
-    </tr>
-    <tr bgcolor="#c9e5f2">
-      <td colspan="2" align="center" style="border:1px solid #fff;">
-        <span style="font-weight:bold;margin-left:20px;font-size:10px;letter-spacing:1.5;color:#336699;">FLAGSTAFF</span>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <div class="adinfodiv1">
+        <p style="font-weight:bold;font-size:1em;color:#336699;">FLAGSTAFF</p>
+        <div style="margin:10px auto 80px;">
             <span style="font-weight:bold;">Clay McCauslin</span><br />(928) 556-2642<br />
             <a href="mailto:clay.mccauslin@kaff.com">clay.mccauslin@kaff.com</a>
         </div>
       </td>
       <td>
-        <div class="adinfodiv1">
-            <span style="font-weight:bold;">Anthony DeFazio</span><br />(928) 556-2652<br />
-            <a href="mailto:anthony.defazio@kaff.com">anthony.defazio@kaff.com</a>
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <div class="adinfodiv1">
-            <span style="font-weight:bold;">Allison Riggs</span><br />(928) 556-2644<br />
-            <a href="mailto:allison.riggs@kaff.com">allison.riggs@kaff.com</a>
+          <p style="font-weight:bold;font-size:1em;color:#336699;">PRESCOTT</p>
+          <div style="margin:10px auto 80px;">
+            <span style="font-weight:bold;">Patrick Gallagher</span><br />(928) 445-6880 ext 1<br />
+            <a href="mailto:patrick@magic991.com">patrick@magic991.com</a>
         </div>
       </td>
       <td>
-        <div class="adinfodiv1">
-                &nbsp;
-        </div>
-      </td>
-    </tr>
-    <tr bgcolor="#c9e5f2">
-        <td colspan="2" align="center" style="border:1px solid #fff;">
-            <span style="font-weight:bold;margin-left:20px;font-size:10px;letter-spacing:1.5;color:#336699;">NATIONAL</span>
-        </td>
-    </tr>
-      <td>
-        <div class="adinfodiv1">
-            <span style="font-weight:bold;">Janie Richardson</span><br />(623) 249-3048<br />
-            <a href="mailto:janie.richardson@kaff.com">janie.richardson@kaff.com</a>
-        </div>
-      </td>
-      <td>
-        <div class="adinfodiv1">
-            <br/>
-        </div>
+          <p style="font-weight:bold;font-size:1em;color:#336699;">NATIONAL</p>
+          <div style="margin:10px auto 80px;">
+            <span style="font-weight:bold;">Steve Hoshor</span><br />(928) 556-2636<br />
+            <a href="mailto:steve.hoshor@kaff.com">steve.hoshor@kaff.com</a>
+          </div>
       </td>
     </tr>
 </table>
